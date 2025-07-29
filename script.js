c7a15c16ebad2aadc571ba06e6ad4ae7bbc3e094
wonyoungE <error071312@gmail.com>
2025-07-29 22:48:30 +0900

    feat: 게시판 html, css

---
+++ script.js
@@ -0,0 +1,4 @@
+const API_BASE_URL = "https://jsonplaceholder.typicode.com";
+
+const postListContainer = document.querySelector("#postListContainer");
+const postDetailContainer = document.querySelector("#postDetailContainer");
